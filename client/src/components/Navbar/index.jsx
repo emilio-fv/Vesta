e433264c4148960b9296b217c9dd4cd87078145b
emilio-fv <emiliofv.sd@gmail.com>
2023-05-04 17:01:03 -0700

    set up account modal

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -13,11 +13,19 @@ import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded
 import Tooltip from '@mui/material/Tooltip';
 import { Link as RouterLink } from 'react-router-dom';
 import Link from '@mui/material/Link';
+import Modal from '@mui/material/Modal';
+import Register from '../Register';
 
 const categories = ['Unisex', 'Women', 'Men'];
 
 const Navbar = () => {
   const [anchorElNav, setAnchorElNav] = useState(null);
+  
+  // Account Button
+  const [accountOpen, setAccountOpen] = useState(false);
+  const handleAccountOpen = () => setAccountOpen(true);
+  const handleAccountClose = () => setAccountOpen(false);
+
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -28,132 +36,152 @@ const Navbar = () => {
   };
 
   return (
-    <AppBar position='static'>
-      <Container maxWidth='xl'>
-        <Toolbar disableGutters
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-between'
-          }}
-        >
-          {/* Logo (Desktop) */}
-          <Link 
-            component={RouterLink} 
-            to='/' 
-            underline='none' 
-            variant='h6'
-            noWrap
-            // TODO: handle link click
+    <>
+      <AppBar position='static'>
+        <Container maxWidth='xl'>
+          <Toolbar disableGutters
             sx={{
-              display: { xs: 'none', md: 'flex' },
-              color: 'white'
+              display: 'flex',
+              justifyContent: 'space-between'
             }}
           >
-            VESTA
-          </Link>
-          {/* Menu (Mobile) */}
-          <Box 
-            sx={{ 
-              display: { xs: 'flex', md: 'none' },
-            }}
-          >
-            <IconButton
-              size='large'
-              aria-label='product categories menu'
-              aria-controls='menu-appbar'
-              aria-haspopup='true'
-              onClick={handleOpenNavMenu}
-            >
-              <MenuIcon htmlColor='#fff'/>
-            </IconButton>
-            <Menu
-              id='menu-appbar'
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
+            {/* Logo (Desktop) */}
+            <Link 
+              component={RouterLink} 
+              to='/' 
+              underline='none' 
+              variant='h6'
+              noWrap
+              // TODO: handle link click
               sx={{
-                display: { xs: 'block', md: 'none' }
+                display: { xs: 'none', md: 'flex' },
+                color: 'white'
               }}
             >
-              {categories.map((category) => (
-                <MenuItem
-                  key={category}
-                  onClick={handleCloseNavMenu}
-                >
-                  <Link 
-                    onClick={() => console.log(category)}
-                    component={RouterLink}
-                    to='/products'
-                    underline='none'
-                    noWrap
-                    // TODO: handle link click
-                  >
-                    {category}
-                  </Link>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          {/* Logo (Mobile) */}
-          <Link
-            component={RouterLink}
-            to='/'
-            noWrap
-            underline='none'
-            sx={{
-              display: { xs: 'flex', md: 'none' },
-              color: 'white'
-            }}
-          >
-            VESTA
-          </Link>
-          {/* Menu (Desktop) */}
-          <Box sx={{ marginLeft: 3, flexGrow: 1, display: { xs: 'none', md: 'flex' }, gap: 3 }}>
-            {categories.map((category) => (
-              <Link 
-                key={category}
-                component={RouterLink}
-                to='/products'
-                noWrap
-                underline='none'
-                // TODO: Handle click
-                sx={{ color: 'white' }}
+              VESTA
+            </Link>
+            {/* Menu (Mobile) */}
+            <Box 
+              sx={{ 
+                display: { xs: 'flex', md: 'none' },
+              }}
+            >
+              <IconButton
+                size='large'
+                aria-label='product categories menu'
+                aria-controls='menu-appbar'
+                aria-haspopup='true'
+                onClick={handleOpenNavMenu}
               >
-                {category}
-              </Link>
-            ))}
-          </Box>
-          {/* Icons (Mobile & Desktop) */}
-          <Box sx={{ flexGrow: 0 }}>
-            {/* TODO: Add links to icons */}
-            <Tooltip title="Account">
-              <IconButton>
-                <AccountCircleIcon fontSize='small' htmlColor='#fff'/>
+                <MenuIcon htmlColor='#fff'/>
               </IconButton>
-            </Tooltip>
-            <Tooltip title="Favorites">
-              <IconButton>
-                <FavoriteBorderRoundedIcon fontSize='small' htmlColor='#fff'/>
-              </IconButton>
-            </Tooltip>
-            <Tooltip title="Shopping Cart">
-              <IconButton>
-                <ShoppingCartRoundedIcon fontSize='small' htmlColor='#fff'/>
-              </IconButton>
-            </Tooltip>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+              <Menu
+                id='menu-appbar'
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' }
+                }}
+              >
+                {categories.map((category) => (
+                  <MenuItem
+                    key={category}
+                    onClick={handleCloseNavMenu}
+                  >
+                    <Link 
+                      onClick={() => console.log(category)}
+                      component={RouterLink}
+                      to='/products'
+                      underline='none'
+                      noWrap
+                      // TODO: handle link click
+                    >
+                      {category}
+                    </Link>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            {/* Logo (Mobile) */}
+            <Link
+              component={RouterLink}
+              to='/'
+              noWrap
+              underline='none'
+              sx={{
+                display: { xs: 'flex', md: 'none' },
+                color: 'white'
+              }}
+            >
+              VESTA
+            </Link>
+            {/* Menu (Desktop) */}
+            <Box sx={{ marginLeft: 3, flexGrow: 1, display: { xs: 'none', md: 'flex' }, gap: 3 }}>
+              {categories.map((category) => (
+                <Link 
+                  key={category}
+                  component={RouterLink}
+                  to='/products'
+                  noWrap
+                  underline='none'
+                  // TODO: Handle click
+                  sx={{ color: 'white' }}
+                >
+                  {category}
+                </Link>
+              ))}
+            </Box>
+            {/* Icons (Mobile & Desktop) */}
+            <Box sx={{ flexGrow: 0 }}>
+              {/* TODO: Add links to icons */}
+              <Tooltip title="Account">
+                <IconButton onClick={handleAccountOpen}>
+                  <AccountCircleIcon fontSize='small' htmlColor='#fff'/>
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Favorites">
+                <IconButton>
+                  <FavoriteBorderRoundedIcon fontSize='small' htmlColor='#fff'/>
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Shopping Cart">
+                <IconButton>
+                  <ShoppingCartRoundedIcon fontSize='small' htmlColor='#fff'/>
+                </IconButton>
+              </Tooltip>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Modal
+        open={accountOpen}
+        onClose={handleAccountClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          bgcolor: 'white',
+          transform: 'translate(-50%, -50%)',
+          width: 400,
+          height: 400
+        }}>
+          <Register />
+        </Box>
+      </Modal>
+    </>
   )
 }
 
---
+++ client/src/components/Register/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const Register = () => {
+    return (
+        <Box>
+            <Typography>Register Form</Typography>
+        </Box>
+    )
+}
+
+export default Register;
