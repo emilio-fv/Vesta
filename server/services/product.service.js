eb05477abfbfa6593e85c961f25f27b219e4fbe4
emilio-fv <emiliofv.sd@gmail.com>
2023-05-16 12:10:13 -0700

    product api services

---
+++ server/services/product.service.js
@@ -0,0 +1,58 @@
+// Import Product model
+const { Product } = require('../models/product.model');
+
+// Create Product
+const createProduct = async (data) => {
+    console.log("Service: createProduct");
+    const newProduct = await Product.create(data);
+    return newProduct;
+}
+
+// Get All Products
+const getAllProducts = async () => {
+    console.log("Service: getAllProducts");
+    const allProducts = await Product.findAll();
+    return allProducts;
+}
+
+// Get Products By Category
+const getProductsByCategory = async (category) => {
+    console.log("Service: getProductsByCategory");
+    const allProducts = await Product.findAll({
+        where: {
+            category: category
+        }
+    })
+    return allProducts;
+}
+
+// Update Product By Id
+const updateProductById = async (data, id) => {
+    console.log("Service: updateProductById");
+    const updatedProduct = await Product.update(data, {
+        where: {
+            id: id
+        }
+    })
+    return updatedProduct;
+}
+
+// Delete Product By Id
+const deleteProductById = async (id) => {
+    console.log("Service: deleteProductById");
+    const numOfDeletedProducts = await Product.destroy({
+        where: {
+            id: id
+        }
+    })
+    return numOfDeletedProducts;
+}
+
+// Exports
+module.exports = {
+    createProduct: createProduct,
+    getAllProducts: getAllProducts,
+    getProductsByCategory: getProductsByCategory,
+    updateProductById: updateProductById,
+    deleteProductById: deleteProductById
+}
