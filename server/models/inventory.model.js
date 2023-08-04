0e9336ebc3ba3b09b4d9e4350131c7c44cdace3b
emilio-fv <emiliofv.sd@gmail.com>
2023-08-03 17:27:26 -0700

    inventory model

---
+++ server/models/inventory.model.js
@@ -0,0 +1,65 @@
+// Imports
+const { DataTypes } = require('sequelize');
+const { db } = require('../config/db.config');
+const { Product } = require('./product.model');
+
+// Inventory model
+const Inventory = db.define('Inventory', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    size: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    color: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    onSale: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    discount: {
+        type: DataTypes.DECIMAL(3,2),
+        defaultValue: 0,
+        validate: {
+            isDecimal: {
+                args: true,
+                msg: "Valid discount required."
+            },
+            isPercentage(value) {
+                if (parseInt(value) < 0 || parseInt(value) >= 1) {
+                    throw new Error('Discount must be between 0% and 100%.');
+                }
+            }
+        }
+    },
+    featured: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+},
+{
+    underscored: true,
+    tableName: 'inventory',
+});
+
+// Define relationship to Products
+Inventory.belongsTo(Product, { foreignKey: 'productId' })
+
+// Update database
+Inventory.sync().then(() => {
+    console.log('Inventory model synced');
+});
+
+// Exports
+module.exports = {
+    Inventory
+}
